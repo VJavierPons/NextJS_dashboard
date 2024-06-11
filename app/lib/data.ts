9c32bf8f2bd8b72ebbd3dd7bfffb1c88bf9f0bd9
Javier Pons <javier.pons@tietoevry.com>
2024-06-11 10:42:06 +0300

    new changes: fetching data, uncomment

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -229,3 +229,18 @@ export async function getUser(email: string) {
     throw new Error('Failed to fetch user.');
   }
 }
+
+export async function latestInvoices() {
+  try {
+    // Fetch the last 5 invoices, sorted by date
+    const data = await sql<LatestInvoiceRaw>`
+      SELECT invoices.amount, customers.name, customers.image_url, customers.email
+      FROM invoices
+      JOIN customers ON invoices.customer_id = customers.id
+      ORDER BY invoices.date DESC
+      LIMIT 5`;
+  } catch (error) {
+    console.error('Failed to get latest invoices');
+    throw new Error('Failed to get latest invoices');
+  }
+}
